25ecb814a3e64e5e04d5fdcda4c8406da5b42566
Shelby Slosar <shelby.slosar@protonmail.com>
2022-06-18 14:13:41 -0700

    first compiled project

---
+++ first_react_project/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App d-flex flex-column w-25 mx-auto mt-4">
+              <h1 className='text-center'>Hello Dojo</h1>
+              <h4>To do list:</h4>
+              <ul className='text-start'>
+                <li>Learn React</li>
+                <li>Cure Cancer</li>
+                <li>Get a Job</li>
+                <li>Figure out what "Relax" means?</li>
+              </ul>
+    </div>
+  );
+}
+
+
+export default App;
---
+++ first_react_project/src/components/comp.jsx
@@ -0,0 +1,11 @@
+class App extends Component {
+        render(){
+            return (
+                <>
+                    <h1>Hello World</h1>
+                    <p>This is a paragraph</p>
+                </>
+            );
+        }
+    }
+    

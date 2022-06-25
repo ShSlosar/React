4c9d9400524b1434819767ce54df0bec3cdf1a91
Shelby Slosar <shelby.slosar@protonmail.com>
2022-06-25 08:47:06 -0700

    validation & conditional rendering

--- big_inversion/src/components/functFormComp.jsx
+++ big_inversion/src/components/functFormComp.jsx
@@ -4,21 +4,51 @@ const UserForm = (props) => {
     const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [usernameError, setUserNameError] = useState("")
+    const [hasBeenSubmitted, setHasBeenSubmitted] = useState(false);
 
+    const handleUserName =(e)=> {
+        setUsername(e.target.value);
+        if(e.target.value.length < 3) {
+            setUserNameError("username must be longer")
+        }
+        else{
+            setUserNameError("");
+        }
+    }
     const createUser = (e) => {
         e.preventDefault();
         const newUser = {username, email, password};
-
         setUsername("");
         setEmail("");
         setPassword("");
         console.log("Welcome User! ", newUser);
+        setHasBeenSubmitted(true);
+    };
+    const formMessage = () => {
+        if(hasBeenSubmitted){
+            return "Thanks for submitting the form!";
+        }
+        else{
+            return "Please submit the form.";
+        }
     };
     return(
         <form onSubmit={ createUser }>
+            {
+                hasBeenSubmitted ?
+                <h3>Thanks for stopping by!</h3>:
+                <h3>Welcome!</h3>
+            }
+            <h3>{formMessage()}</h3>
             <div>
+                {
+                    usernameError ?
+                    <p style={{color:'red'}} >{usernameError}</p>:
+                    ''
+                }
                 <label>Username: </label> 
-                <input type="text" onChange={ (e) => setUsername(e.target.value) } value={username} />
+                <input type="text" onChange={ (e) => handleUserName(e) } value={username} />
             </div>
             <div>
                 <label>Email Address: </label> 

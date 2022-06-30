c5308d7d147ef21cba90d6a6662623bf70ed81d8
Shelby Slosar <shelby.slosar@protonmail.com>
2022-06-30 10:38:11 -0700

    Reducer

---
+++ react_forms/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ReducerComp from './components/ReducerComp';
+
+function App() {
+  return (
+    <div className="App">
+      <ReducerComp />
+    </div>
+  );
+}
+
+export default App;

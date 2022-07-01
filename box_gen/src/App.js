e657ef73067d9c31e2a14cdb6ec2697c97774a2d
Shelby Slosar <shelby.slosar@protonmail.com>
2022-07-01 13:20:21 -0700

    map

---
+++ box_gen/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Groceries from './components/mapComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <Groceries />
+    </div>
+  );
+}
+
+export default App;

0a16fd79370a90d8d8bad4b2ad6ff30fa111da11
Shelby Slosar <shelby.slosar@protonmail.com>
2022-07-04 18:02:46 -0700

    lifting state

--- box_gen/src/App.js
+++ box_gen/src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Drinks from './components/mapProps';
 import Groceries from './components/mapComponent';
+import MessageForm from './components/liftingStateComp';
+import MessageDisplay from './components/messageDisplay';
 
 function App() {
+  const [currentMsg, setCurrentMsg] = useState("No messages yet...");
+  const messageArrived = (newMessage) => {
+    setCurrentMsg(newMessage);
+  }
+
   return (
     <div className="App">
       <Groceries />
       <Drinks drinkList={["Club Soda", "Coke", "Water", "Flavor Aid"]} />
+      <MessageForm onNewMessage={ messageArrived } />
+      <MessageDisplay message={currentMsg} />
     </div>
   );
 }
